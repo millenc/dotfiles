7a30489df7842281720af541106859cdb4caa3c5
Mikel Pintor <mpintor@das-nano.com>
2019-04-20 20:09:39 +0200

    Add cht.sh and bash aliases

---
+++ bash/.bash_aliases
@@ -0,0 +1,3 @@
+# Useful aliases
+
+alias cheat=cht.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 function install_apt_dependencies {
-    echo "Installing basic dependencies with apt..."
-    sudo apt update && \
-        sudo apt install -y git emacs25 vim mercurial stow
+	echo "Installing basic dependencies with apt..."
+	sudo apt update && \
+		sudo apt install -y curl git emacs25 vim mercurial stow
+}
+
+function install_cht_sh {
+	echo "Installing cht.sh command line client..."
+
+	curl https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh > /dev/null
+    	sudo chmod +x /usr/local/bin/cht.sh	
 }
 
 function stow_config {
@@ -18,6 +25,7 @@ function main {
 	echo "Bootstraping your system configuration!"
 
 	install_apt_dependencies
+	install_cht_sh
 	stow_config
 }
 
